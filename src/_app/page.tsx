0594c93bda5342cd3c00c416cb334d49f293eb9e
josue <josuerojas.rojas@gmail.com>
2024-04-23 15:35:48 -0400

    [BUGFIX] Fix build; fix tsconfig

--- src/_app/page.tsx
+++
@@ -1,8 +0,0 @@
-import home from '../md/home.md';
-import { PageMarkdown } from '../component/PageMarkdown';
-
-const Home = () => {
-  return <PageMarkdown mdSource={home} />
-}
-
-export default Home;
--- src/_app/survivalkits/page.tsx
+++
@@ -1,8 +0,0 @@
-import survivalKits from '../../md/survivalkits.md';
-import { PageMarkdown } from "../../component/PageMarkdown";
-
-const SurvivalKits = () => {
-  return <PageMarkdown mdSource={survivalKits} />
-};
-
-export default SurvivalKits;
